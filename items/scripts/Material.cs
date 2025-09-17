09811ff1db968bc0cf574b71208af4057990e18b
Jack Scisco <jackscisco@gmail.com>
2025-09-17 07:10:32 -0400

    Fixup enum management

--- items/scripts/Material.cs
+++ items/scripts/Material.cs
@@ -6,16 +6,19 @@ namespace roguelikeidler.items.scripts
     public partial class Material : Resource 
     {
         [Export] public MaterialType MaterialType { get; set; }
-        [Export] public float ValueMultiplier { get; set; }
+        [Export] public float ValueMultiplier { get; set; } = 1.0f;
 
         public Material()
         {
         }
     }
 
+    // Set each value explicitly to support adding new values in the middle.
+    // By setting the values like this, we can add 99 materials in between easily.
     public enum MaterialType
     {
-        Iron,
-        Steel
+        Dummy = 0,
+        Iron = 100,
+        Steel = 200,
     }
 }
